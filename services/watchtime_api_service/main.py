d2edbe5dd954c437d2e0bd1448475d139b9e2e74
Ali Choopani Zanjani <alichz2001@gmail.com>
2024-04-30 15:59:41 +0330

    base watchtime_api_service service

--- services/watchtime_api_service/main.py
+++ services/watchtime_api_service/main.py
@@ -1,27 +1,39 @@
 import psycopg2
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 def get_db_connection():
-    conn = psycopg2.connect(host='postgres',
-                            database='royanegar',
-                            user='royamegar_user',
+    conn = psycopg2.connect(host='localhost',
+                            database='postgres',
+                            user='royanegar_user',
                             password='1234')
     return conn
 
 
 @app.route('/api/v1/watchtime/user')
-def index():
+def user_watchtime():
+    args = request.args
+    req_user_id = args.get('id', default="", type=str)
 
-    req_user_id = args.get("id", default=0, type=int)
-
-    
+    if req_user_id == "" :
+        return "bad username", 400
 
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('SELECT * FROM books;')
-    books = cur.fetchall()
+    cur.execute('SELECT duration FROM user_watchtimes where "user" = %s;', (req_user_id,))
+    user_watchtime = cur.fetchone()
     cur.close()
     conn.close()
-    return render_template('index.html', books=books)
+
+    if user_watchtime == None:
+        return "0", 200
+    
+    return str(user_watchtime[0]), 200
+
+
+if __name__ == "__main__":
+
+    app.run(host='0.0.0.0', port=8001)
+    
+    print("Server stopped.") 
