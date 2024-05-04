94a36cf2a0d974b56923c39380d6c44b477e325b
Ali Choopani Zanjani <alichz2001@gmail.com>
2024-05-04 20:00:20 +0330

    all works execpt haproxy.

--- services/watchtime_aggregator_service/conn.py
+++ services/watchtime_aggregator_service/conn.py
@@ -8,6 +8,11 @@ DB_PG_USER = os.environ['DB_PG_USER']
 DB_PG_PASSWORD = os.environ['DB_PG_PASSWORD']
 DB_PG_DATABASE_NAME = os.environ['DB_PG_DATABASE_NAME']
 
+DB_CH_USER = os.environ['DB_CH_USER']
+DB_CH_PASSWORD = os.environ['DB_CH_PASSWORD']
+DB_CH_HOSTNAME = os.environ['DB_CH_HOSTNAME']
+DB_CH_PORT = os.environ['DB_CH_PORT']
+DB_CH_DATABASE_NAME = os.environ['DB_CH_DATABASE_NAME']
 
 def get_pg_connection():
     conn_str = "postgresql://%s:%s@%s:%s/%s" % (
@@ -17,9 +22,16 @@ def get_pg_connection():
         DB_PG_PORT,
         DB_PG_DATABASE_NAME
     )
+    print(conn_str)
     conn = psycopg.connect(conn_str)
     return conn
 
 def get_ch_connection():
-    conn = clickhouse_connect.get_client(host='localhost')
+    conn = clickhouse_connect.get_client(
+        host=DB_CH_HOSTNAME,
+        port=DB_CH_PORT,
+        username=DB_CH_USER,
+        password=DB_CH_PASSWORD,
+        database=DB_CH_DATABASE_NAME
+        )
     return conn
